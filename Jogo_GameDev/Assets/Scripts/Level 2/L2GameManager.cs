3d6e78ee5abe18132da40cd6713a745cadcb3fdf
brunosecchin <brunosecchin@gmail.com>
2025-06-03 18:32:29 -0300

    lvl2 mecanics and rain

--- Jogo_GameDev/Assets/Scripts/Level 2/L2GameManager.cs
+++ Jogo_GameDev/Assets/Scripts/Level 2/L2GameManager.cs
@@ -10,32 +10,59 @@ public class L2GameManager : MonoBehaviour
     public bool isGameActive;
     public GameObject titleScreen;
     public GameObject gameOverScreen;
+    public GameObject endLevelScreen;
     private L2GameManager gameManager;
+    public Button prev_button;
     public Button start_button;
     public Button restart_button;
+    public Button restartGO_button;
     public Button next_button;
+    private GameObject mainCamera;
+    private Vector3 cameraPosition;
+    private bool moveCamera = false;
     // Start is called before the first frame update
     void Start()
     {
+        prev_button.onClick.AddListener(PrevGame);
         start_button.onClick.AddListener(StartGame);
         restart_button.onClick.AddListener(RestartGame);
+        restartGO_button.onClick.AddListener(RestartGame);
         next_button.onClick.AddListener(NextGame);
+        mainCamera = GameObject.Find("Main Camera");
+        cameraPosition = mainCamera.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (moveCamera)
+        {
+            mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, cameraPosition, 5.0f * Time.deltaTime);
+            if (Vector3.Distance(mainCamera.transform.position, cameraPosition) < 0.01f)
+            {
+                moveCamera = false;
+            }
+        }
     }
-    
-    public void StartGame(){
+
+    public void StartGame()
+    {
         titleScreen.gameObject.SetActive(false);
         isGameActive = true;
     }
-    public void GameOver()
+    public IEnumerator GameOver()
     {
-        gameOverScreen.gameObject.SetActive(true);
         isGameActive = false;
+        cameraPosition = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y, -10.5f);
+        moveCamera = true;
+        yield return new WaitForSeconds(1.0f);
+        gameOverScreen.gameObject.SetActive(true);
+    }
+
+    public void EndLevel()
+    {
+        isGameActive = false;
+        endLevelScreen.gameObject.SetActive(true);
     }
     public void RestartGame()
     {
@@ -44,5 +71,9 @@ public class L2GameManager : MonoBehaviour
     public void NextGame()
     {
         SceneManager.LoadScene("LevelThree");
+    }
+    public void PrevGame()
+    {
+        SceneManager.LoadScene("LevelOne");
     }
 }
--- Jogo_GameDev/Assets/Scripts/Level 2/L2PlayerController.cs
+++ Jogo_GameDev/Assets/Scripts/Level 2/L2PlayerController.cs
@@ -9,50 +9,95 @@ public class L2PlayerController : MonoBehaviour
     private Animator playerAnim;
     private AudioSource playerAudio;
     public AudioClip jumpSound;
-    public AudioClip crounchSound;
-    public AudioClip crashSound;
+    public AudioClip crouchSound;
+    public AudioClip deathSound;
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
-    public bool gameOver = false;
+    public float speed;
+    private float xRange = 5.0f;
+    private float horizontalInput;
+    private BoxCollider boxCollider;
+    private Vector3 originalSize;
+    private Vector3 originalCenter;
+    private bool isCrouching = false;
+    private Vector3 crouchedSize = new Vector3(0.8f, 1.4f, 0.5f);
+    private Vector3 crouchedCenter = new Vector3(0f, 0.7f, 0f);
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<L2GameManager>();
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
-        Physics.gravity *= gravityModifier;
+        Physics.gravity = new Vector3(0, -9.81f * gravityModifier, 0);
         playerAudio = GetComponent<AudioSource>();
+        boxCollider = GetComponent<BoxCollider>();
+        if (boxCollider != null)
+        {
+            originalSize = boxCollider.size;
+            originalCenter = boxCollider.center;
+        }
+        else
+        {
+            Debug.Log("BoxCollider não encontrado!");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameManager.isGameActive){
+        if (gameManager.isGameActive)
+        {
             playerAnim.SetBool("Run_b", true);
         }
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && gameManager.isGameActive){
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) && isOnGround && gameManager.isGameActive && !isCrouching)
+        {
+            playerAnim.SetTrigger("Crouch_trig");
+            playerAudio.PlayOneShot(crouchSound, 1.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && gameManager.isGameActive && !isCrouching)
+        {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
             playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) && isOnGround && gameManager.isGameActive){
-            playerAnim.SetTrigger("Crounch_trig");
-            playerAudio.PlayOneShot(crounchSound, 1.0f);
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
     }
-    private void OnCollisionEnter(Collision collision){
-        if ( collision.gameObject.CompareTag("Ground")){
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Ground")) {
             isOnGround = true;
         }
-        else if (collision.gameObject.CompareTag("Obstacle")){
+        else if (other.gameObject.CompareTag("Obstacle")) {
             playerAnim.SetBool("Death_b", true);
-            gameManager.GameOver();
-            Debug.Log("Game Over!");
+            StartCoroutine(gameManager.GameOver());
             // playerAnim.SetInteger("DeathType_int", 1);
-            playerAudio.PlayOneShot(crashSound, 1.0f);
+            playerAudio.PlayOneShot(deathSound, 1.0f);
         }
+    }
+
+    public void Crouch()
+    {
+        isCrouching = true;
+        boxCollider.size = crouchedSize;
+        boxCollider.center = crouchedCenter;
+    }
+    public void EndCrouch()
+    {
+        isCrouching = false;
+        boxCollider.size = originalSize;
+        boxCollider.center = originalCenter;
     }
 }
